92405f05e9ebb75d369f563125d0a4aefa8d45d5
Prince <princeshrestha2111018@gmail.com>
2025-12-10 23:09:51 +0545

    Task 2 IntroPage complete

---
+++ FirstMAUIapp/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FirstMAUIapp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "FirstMAUIapp" };
+        }
+    }
+}
---
+++ FirstMAUIapp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstMAUIapp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
